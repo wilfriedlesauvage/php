1b0331179d7330218fb144803be98de00c065e76
wilfriedlesauvage <obistrip@outlook.fr>
2020-05-25 12:10:56 +0200

    ajout exo6

---
+++ exercice6/index.php
@@ -0,0 +1,29 @@
+<?php
+$number = 140;
+$number += 30;
+$number /= 2;
+?>
+        
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Exo 6 PHP</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+
+      <p> <?php echo $number ?> </p>
+
+    </body>
+</html>
+
+
+
+
